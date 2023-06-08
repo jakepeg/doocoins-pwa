a5f71f0e656ab5b7c1bfa43e088c5efc8a4499eb
Jeet Viramgama <jviramgama5@gmail.com>
2023-06-08 09:34:48 +0530

    PWA audit fixes

--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
-serviceWorkerRegistration.register();
 root.render(<App />);
