01c6f76e6c0cb772c80f2b3e50e9520449454e14
Jeet Viramgama <64732692+jv18creator@users.noreply.github.com>
2023-06-16 15:02:42 +0530

    fix child id issue

--- src/frontend/screens/Tasks.jsx
+++ src/frontend/screens/Tasks.jsx
@@ -8,91 +8,100 @@ const Tasks = () => {
   const [actor, setActor] = React.useState(null);
   const [tasks, setTasks] = React.useState({});
   const [isLoading, setIsLoading] = React.useState(false);
-  const child = "";
+  const [child, setChild] = React.useState(null);
   // const child = "2vxsx-fae-1";
 
-  get('selectedChild').then((data) => {
-    child = data;
-  })
-
+  console.log(`child`, child);
   const childName = "Jake";
   const childBalance = "147";
 
   // const childName = get("selectedChildName");
   // const childBalance = get("balance-" + child);
 
+  React.useEffect(() => {
+    get("selectedChild").then((data) => {
+      setChild(data);
+    });
+  }, [])
 
   function getTasks() {
     if (child) {
-    console.log("getTasks called for child id: "+child);
-    setIsLoading(true);
-    actor?.getTasks(child).then((returnedTasks) => {
-      if ("ok" in returnedTasks) {
-        const tasks = Object.values(returnedTasks);
-        setTasks(tasks);
-        setIsLoading(false);
-      } else {
-        console.error(returnedTasks.err);
-      }
-    });
-    return false;
+      console.log("getTasks called for child id: " + child);
+      setIsLoading(true);
+      actor?.getTasks(child).then((returnedTasks) => {
+        if ("ok" in returnedTasks) {
+          const tasks = Object.values(returnedTasks);
+          setTasks(tasks);
+          setIsLoading(false);
+        } else {
+          console.error(returnedTasks.err);
+        }
+      });
+      return false;
+    }
   }
-}
 
-// add swiper - delete, edit, approve tasks
-function handleTaskComplete(task_id) {
-  let r = window.confirm("Is the task complete?");
-  if (r == true) {
-    let dateNum = Math.floor(Date.now() / 1000);
-    let date = dateNum.toString();
-    // API call approveTask
-    actor?.approveTask(selectedChild,task_id,date).then((returnedApproveTask) => {
-      if ("ok" in returnedApproveTask) {
-        setTaskComplete(parseInt(task_id));
-      } else {
-        console.error(returnedApproveTask.err);
-      }
-    });
-  } else {
-    console.log("You pressed cancel!");
+  // add swiper - delete, edit, approve tasks
+  function handleTaskComplete(task_id) {
+    let r = window.confirm("Is the task complete?");
+    if (r == true) {
+      let dateNum = Math.floor(Date.now() / 1000);
+      let date = dateNum.toString();
+      // API call approveTask
+      actor
+        ?.approveTask(selectedChild, task_id, date)
+        .then((returnedApproveTask) => {
+          if ("ok" in returnedApproveTask) {
+            setTaskComplete(parseInt(task_id));
+          } else {
+            console.error(returnedApproveTask.err);
+          }
+        });
+    } else {
+      console.log("You pressed cancel!");
+    }
   }
-}
 
-const initActor = () => {
-  import("../../declarations/backend").then((module) => {
-    const actor = module.createActor(module.canisterId, {});
-    setActor(actor);
-  });
-};
+  const initActor = () => {
+    import("../../declarations/backend").then((module) => {
+      const actor = module.createActor(module.canisterId, {});
+      setActor(actor);
+    });
+  };
 
-React.useEffect(() => {
-  initActor();
-}, []);
+  React.useEffect(() => {
+    initActor();
+  }, []);
 
-React.useEffect(() => {
-  getTasks(child);
-}, [actor]);
+  React.useEffect(() => {
+    if (child) getTasks(child);
+  }, [actor, child]);
 
   return (
-<>
-    <Balance 
-      childName = {childName}
-      childBalance = {childBalance}
-    />
+    <>
+      <Balance childName={childName} childBalance={childBalance} />
 
       <div className="light-panel">
-      <h2 className="screen-title dark">Tasks (add button)</h2>
-            {isLoading ? <LoadingSpinner /> : null}
-            {tasks.length > 0 &&
-                tasks[0].map(task => (
-                  <div className="list-item" role="button" key={parseInt(task.id)} onClick={() => handleTaskComplete(parseInt(task.id))} onKeyDown={() => handleTaskComplete(parseInt(task.id))}>
-                    <div>{task.name}</div>
-                    <div><img src={dc} className="dc-img-small" alt="DooCoins symbol" />{parseInt(task.value)}</div>
-                  </div> 
-                ))
-            }
+        <h2 className="screen-title dark">Tasks (add button)</h2>
+        {isLoading ? <LoadingSpinner /> : null}
+        {tasks.length > 0 &&
+          tasks[0].map((task) => (
+            <div
+              className="list-item"
+              role="button"
+              key={parseInt(task.id)}
+              onClick={() => handleTaskComplete(parseInt(task.id))}
+              onKeyDown={() => handleTaskComplete(parseInt(task.id))}
+            >
+              <div>{task.name}</div>
+              <div>
+                <img src={dc} className="dc-img-small" alt="DooCoins symbol" />
+                {parseInt(task.value)}
+              </div>
+            </div>
+          ))}
       </div>
-      </>
+    </>
   );
 };
 
