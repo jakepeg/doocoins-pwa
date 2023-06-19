f5f7a5d26458e9a85ff1d936a600fdcdb2ea9b2e
jakepeg <jakepeg@gmail.com>
2023-06-19 06:45:18 +0100

    tidy code

--- src/frontend/components/Balance.jsx
+++ src/frontend/components/Balance.jsx
@@ -1,16 +1,7 @@
 import * as React from "react";
-// import { get } from "idb-keyval";
 import dc from "../assets/images/dc-thin-white.svg";
 
 const Balance = (props) => {
-
-  // const childName = get("selectedChildName");
-  // const childID = get("selectedChild");
-  // const childBalance = get("balance-" + childID);
-
-  // const childName = "Jake";
-  // const childID = "abc-123";
-  // const childBalance = "147";
 
   return (
       <div className={props.isModalOpen}>
