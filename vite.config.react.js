884e96d34f67b2b60e3a7136800642ad762dd268
jakepeg <jakepeg@gmail.com>
2024-03-04 15:17:23 +0000

    fix Connection refused (os error 61)

--- vite.config.react.js
+++ vite.config.react.js
@@ -69,6 +69,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "http://127.0.0.1:4943",
+        // target: "http://127.0.0.1:5173",
         changeOrigin: true
       }
     }
