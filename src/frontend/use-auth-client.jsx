742411f1d763eb12b12c7395d467e73a62525e31
Jeet Viramgama <jviramgama5@gmail.com>
2024-11-30 04:53:31 -0600

    fix: remove logs & dead code
    Signed-off-by: Jeet Viramgama <jviramgama5@gmail.com>

--- src/frontend/use-auth-client.jsx
+++ src/frontend/use-auth-client.jsx
@@ -23,58 +23,6 @@ import { idlFactory } from "../declarations/backend";
 
 const AuthContext = createContext();
 
-function useWhyDidYouUpdate(name, props) {
-  // create a reference to track the previous data
-  const previousProps = useRef();
-
-  useEffect(() => {
-    if (previousProps.current) {
-      // merge the keys of previous and current data
-      const keys = Object.keys({ ...previousProps.current, ...props });
-
-      // to store what has change
-      const changesObj = {};
-
-      // check what values have changed between the previous and current
-      keys.forEach((key) => {
-        // if both are object
-        if (
-          typeof props[key] === "object" &&
-          typeof previousProps.current[key] === "object"
-        ) {
-          if (
-            JSON.stringify(previousProps.current[key]) !==
-            JSON.stringify(props[key])
-          ) {
-            // add to changesObj
-            changesObj[key] = {
-              from: previousProps.current[key],
-              to: props[key],
-            };
-          }
-        } else {
-          // if both are non-object
-          if (previousProps.current[key] !== props[key]) {
-            // add to changesObj
-            changesObj[key] = {
-              from: previousProps.current[key],
-              to: props[key],
-            };
-          }
-        }
-      });
-
-      // if changesObj not empty, print the cause
-      if (Object.keys(changesObj).length) {
-        console.log("This is causing re-renders", name, changesObj);
-      }
-    }
-
-    // update the previous props with the current
-    previousProps.current = props;
-  });
-}
-
 export const AuthProvider = ({ children }) => {
   const [actor, setActor] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
@@ -93,36 +41,10 @@ export const AuthProvider = ({ children }) => {
     fetchRootKey: isLocal,
   });
 
-  // useEffect(() => {
-  //   console.log(`authenticatedAgent`, authenticatedAgent);
-  //   if (authenticatedAgent) {
-  //     setIsLoading(true);
-
-  //     console.log("isLocal: ", isLocal);
-  //     // const agent = new HttpAgent({
-  //     //   host: isLocal ? "http://localhost:4943" : "https://icp-api.io",
-  //     //   identity: identityKit.identity,
-  //     //   verifyQuerySignatures: !isLocal,
-  //     // });
-
-  //     // const newActor = Actor.createActor(idlFactory, { agent, canisterId })
-  //     const newActor = Actor.createActor(idlFactory, {
-  //       agent: authenticatedAgent,
-  //       canisterId,
-  //     });
-
-  //     setActor(newActor);
-  //     setIsLoading(false);
-  //   } else {
-  //     setIsLoading(false);
-  //   }
-  // }, [authenticatedAgent, isLocal]);
-
   useEffect(() => {
     async function initAgent() {
       if (authenticatedAgent) {
         setIsLoading(true);
-        console.log("isLocal: ", isLocal);
 
         try {
           // Fetch root key if in local development
@@ -155,8 +77,6 @@ export const AuthProvider = ({ children }) => {
   }, [connect]);
 
   const logout = useCallback(async () => {
-    console.log(`not supposed to be here`);
-
     // await identityKit.disconnect();
     await disconnect();
     del("childList");
@@ -193,8 +113,6 @@ export const AuthProvider = ({ children }) => {
     login,
     logout,
   ]);
-
-  console.log(`main runs`, authValue);
 
   return (
     <AuthContext.Provider value={authValue}>{children}</AuthContext.Provider>
